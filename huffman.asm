d215f77ed6139a51fde1838bc84c1199e9cae8c3
Ion <ion.toncu@student.upt.ro>
2021-11-24 01:40:19 +0200

    write one fun

--- huffman.asm
+++ huffman.asm
@@ -4,22 +4,39 @@
 
 org 100h  
 .data 
-   MAX_TREE_HT  db 100  
+;   initial data
+    arr db 'a', 'b', 'c', 'd', 'e', 'f'
+    freq db 5, 9, 12, 13, 16, 45
+    size db 6
 ;  MinHeapNode 
     MinHeapNode_char_Data db 100 dup(?)   
     MinHeapNode_unsigned_freq db 100 dup(?) 
     MinHeapNode_MinHeapNode_left_index db 100 dup(?) 
     MinHeapNode_MinHeapNode_right_index db 100 dup(?)
 ;  MinHeap (or Huffman tree) 
-    MinHeap_unsigned_size db 100 dup(?) 
+    MinHeap_unsigned_size db 0 
     MinHeap_unsigned_capacity db 100 dup(?) 
-    MinHeap_MinHeapNode_index db 100 dup(?)
+    MinHeap_MinHeapNode_array db 100 dup(?)
+;  HuffmanCodes fun var
+    HuffmanCodes_arr db 100 dup(?)
+;  buildHuffmanTree fun var
+    buildHuffmanTree_left db 0
+    buildHuffmanTree_right db 0
+    buildHuffmanTree_top db 0
+    buildHuffmanTree_isSizeOne db 0
+;   createAndBuildMinHeap fun var
+    createAndBuildMinHeap_minHeap db 0
+;   extractMin fun var
+    extractMin_tmp dw 0
+;   minHeapify fun var
+    minHeapify_idx dw 0
+    minHeapify_smallest dw 0
+    minHeapify_left dw 0
+    minHeapify_right dw 0
 
 .code
 main proc
-    ;call HuffmanCodes
-   
-
+    call HuffmanCodes
 endp
     
 newNode:
@@ -28,8 +45,10 @@ newNode:
     ret
     
 createMinHeap:
-    xor ax, ax
-
+    mov ah, 0
+    mov MinHeap_unsigned_size, ah
+    mov ah, size
+    mov MINHEAP_UNSIGNED_CAPACITY, ah
     ret
      
 swapMinHeapNode:  
@@ -37,8 +56,29 @@ swapMinHeapNode:
 
     ret  
 
-HuffmanCodes: 
-    xor ax, ax 
+minHeapify
+    pop ax
+    mov minHeapify_idx, ax
+    mov minheapify_smallest, ax
+    inc ax
+    mul bx, ax, 2
+    mov minheapify_left, bx
+    inc ax
+    mul bx, ax, 2
+    mov [minheapify_right], bx   
+    
+    mov ax, [minheapify_left]
+    mov bx, [minheap_unsigned_size]
+    cmp bx, ax
+    jle MHBR1 
+        ;Todo
+MHBR1
+
+    
+
+    ret  
+
+HuffmanCodes:  
     call buildHuffmanTree
     call printCodes
 
@@ -47,9 +87,26 @@ HuffmanCodes:
 buildHuffmanTree:  
     xor ax, ax
     call createAndBuildMinHeap 
-    call isSizeOne
 
+ BHTL1:
+    call isSizeOne
+    call extractMin
+
+    mov ah, buildHuffmanTree_isSizeOne
+    cmp ah, #1
+    jne BHTL1
+    
+
+    call extractMin
+    call newNode
+    call insertMinHeap
+    call extractMin
     ret
+
+buildMinHeap
+    xor ax, ax
+
+    ret 
         
 printCodes:  
     xor ax, ax
@@ -57,7 +114,7 @@ printCodes:
     ret  
 
 createAndBuildMinHeap:  
-    xor ax, ax
+
 
     ret 
 
@@ -67,9 +124,37 @@ isSizeOne:
     ret 
 
 extractMin:  
+    mov ax, [MinHeap_MinHeapNode_array]
+    mov extractmin_tmp, ax
+    mov ax, MinHeap_unsigned_size
+    dec ax
+    mov ax, [MinHeap_MinHeapNode_array + ax]
+    mov [minheap_minheapnode_array], ax
+    mov ax, MinHeap_unsigned_size
+    dec ax
+    mov MinHeap_unsigned_size, ax
+    mov ax, 0
+    push ax
+    call minHeapify
+
+
+    ret 
+
+insertMinHeap:
     xor ax, ax
 
     ret 
+
+printArr:
+    xor ax, ax
+
+    ret 
+isLeaf:
+    xor ax, ax
+
+    ret 
+
+
 
 
 
